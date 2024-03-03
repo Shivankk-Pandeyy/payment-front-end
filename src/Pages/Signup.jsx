4839cd4bbcc6f228499d7a79224e9ccaf6bccb17
Shivank Pandey <125184309+Shivankk-Pandeyy@users.noreply.github.com>
2024-03-03 14:40:14 +0530

    Update Signup.jsx

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -7,6 +7,7 @@ import Aos from "aos"
 import 'aos/dist/aos.css'
 import { useEffect, useState } from "react"
 const Signup = () => {
+    axios.defaults.withCredentials=true
     const navigate=useNavigate();
     const [user,setUser]=useState({
         name:"",
